5530d311df9199c2c6a7767f20c85204b6105de4
soferdani <soferdani@gmail.com>
2020-09-15 10:58:57 +0300

    first setup

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
-server
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const api = require('./server/routes/api')
+const app = express()
 
 
-//express
-//hanbdelbars
-//body parser
-//mongoose
-//JQERY
-//chart.js
-//
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static(path.join(__dirname, 'node_modules')))
+app.use('/', api)
 
-//** Raout 1  */
-//**TODO   */
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/weather")
+
+const port = process.env.PORT || 3000 
+
+app.listen(port, function (err, res) {
+    console.log('server running on port ' + port);
+})

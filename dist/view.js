0a7117f268e657aac672a389a3e4ce932a2b483f
yuvalotem <yuvalotem@gmail.com>
2020-09-15 12:30:32 +0300

    chars init

--- dist/view.js
+++ dist/view.js
@@ -5,9 +5,9 @@ class Renderer {
     renderMenu(){
         const html = '<span id="delete-menu"><i class="fas fa-times"></i></span>'
         $('#menu-bar').empty().append(html)
-        const source = $('#menu-template').html()
-        const template = Handlebars.compile(source)
-        const newHTML = template()
+        // const source = $('#menu-template').html()
+        // const template = Handlebars.compile(source)
+        const newHTML = "<div>hello</div>"
         $('#page-content').empty().append(newHTML)
     }
 }

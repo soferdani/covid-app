c7767722e1ae502ada6e37d4124a513fede530b1
soferdani <soferdani@gmail.com>
2020-09-15 12:32:58 +0300

    some chinges

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express')
+const app = express()
 const path = require('path')
 const bodyParser = require('body-parser')
+const api = require('./server/Routes/api')
 const mongoose = require('mongoose')
-const api = require('./server/routes/api')
-const app = express()
 
 
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -11,6 +11,7 @@ app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.static(path.join(__dirname, 'node_modules')))
 app.use('/', api)
+
 
 // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/weather")
 
